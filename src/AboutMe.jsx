11201d979859ff459c1fdbe46ce0539be0087319
Juli Storino <julieta_storino@mi.unc.edu.ar>
2024-07-27 15:17:02 -0300

    AboutMe styles finished

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -2,21 +2,12 @@
 function AboutMe(){
     return(
         <div className="AboutMe">
-            <div className="AboutMe-text">
-                <h1>
-                    Hello, I'm Julieta Storino!
-                    <br />
-                    I'm a Computer Science student.
-                </h1>
-                <br />
-                <p>
-                    Welcome to my portfolio! I'm currently studying at the National University of Córdoba (Argentina),
-                    and always looking for new challenges and opportunities to learn. 
-                </p>
-            </div>
-            <div className="AboutMe-img">
-                <img src="/girlworking.png" alt="girlworking"></img>
-            </div>
+            <h1 className="AboutMe-text">
+                <span> Hello, I'm Julieta Storino! <br /> I'm a Computer Science student. <br /> </span>            
+                Welcome to my portfolio! I'm currently studying at the National University of Córdoba (Argentina),
+                and always looking for new challenges and opportunities to learn. 
+            </h1>
+            <img className="AboutMe-img" src="/girlworking.png" alt="girlworking"></img>
         </div>
     )
 }
