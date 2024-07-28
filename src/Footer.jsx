bc07cbf2d09c44208a40b58d96b8c7d550785f74
Juli Storino <julieta_storino@mi.unc.edu.ar>
2024-07-28 02:03:45 -0300

    Footer finished

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,34 @@
 
 function Footer() {
   return (
-    <footer>
-      <p> &copy; {new Date().getFullYear()} MiSite </p>
+    <footer className="foot">
+      <ul className="foot-list">
+        <li>
+          <a target="_blank" href="https://github.com/JulietaStorino">
+            <img className="foot-item" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub"></img>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://x.com/julieta_storino">
+            <img className="foot-item" alt="X" src="https://seeklogo.com/images/T/twitter-x-logo-577BCAE525-seeklogo.com.png?v=638264860160000000"></img>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://codeforces.com/profile/julieta_storino">
+            <img className="foot-item" src="https://www.svgrepo.com/show/330186/codeforces.svg" alt="CoderForces"></img>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://linkedin.com/in/julieta-storino">
+            <img className="foot-item" src="https://cdn.freebiesupply.com/logos/large/2x/linkedin-icon-1-logo-png-transparent.png" alt="LinkedIn"></img>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://instagram.com/julieta_storino/">
+            <img className="foot-item" src="https://cdn-icons-png.flaticon.com/512/87/87390.png" alt="Instagram"></img>
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
