e42891446da0c586fa1e4a601f9ef5640d95ca09
Juli Storino <julieta_storino@mi.unc.edu.ar>
2024-07-28 00:35:20 -0300

    Contact avanced

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,39 +1,21 @@
 
-import React, {useState} from 'react';
-
 function Contact() {
 
-    const [name, setName] = useState("Guest");
-    const [comment, setComment] = useState("");
-
-    function handleNameChange(event){
-        setName(event.target.value);
-    }
-
-    function handleCommentChange(event){
-        setComment(event.target.value);
-    }
-
-    let count = 0; // 'let' means that might be reassigned - if not, use 'const' 
-
-    const handleClick = (name,e) => {
-      count++;
-      console.log(`Button clicked ${count} times by ${name}`);
-      e.target.textContent = "OUCH!"
-    };
 
     return (
         <div className="Contact">
-            <h1><span>Contact</span><br/>Get In Touch. Ask for my resume or see if we can build something amazing together. I'd love to hear from you!</h1>
+            <h1>
+                <span>Contact</span>
+                <br/>
+                Get In Touch. Ask for my resume or see if we can build something amazing together. I'd love to hear from you!
+            </h1>
 
-            <p>Name: {name}</p>
-            
-            <input type="text" value={name} onChange={handleNameChange} />
-            
-            <p>Comment: {comment}</p>
-            <textarea value={comment} onChange={handleCommentChange} placeholder="Leave a comment."/>
-
-            <button className="button" onDoubleClick={(e) => handleClick("Bro",e)}>Click me!</button>
+            <form action="https://formsubmit.co/el/banise" method="POST">
+                <input type="text" placeholder="Full Name" required/>
+                <input type="email" placeholder="Email" required/>
+                <textarea placeholder="Message" required/>
+                <button>Send</button>
+            </form>
         </div>
     );
 }
